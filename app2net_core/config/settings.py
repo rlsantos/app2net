cc12f04252c83a8ba81095296f00f161c96e7026
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2021-03-18 10:42:07 -0300

    Settings updates

--- app2net_core/config/settings.py
+++ app2net_core/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts',
     'infrastructure_handler.apps.InfrastructureHandlerConfig',
+    'netapp_manager',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
